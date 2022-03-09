93e32df70cdeb84e012ba295c7b0b7f2a996e728
Rog3rSm1th <r0g3r5@protonmail.com>
2022-03-10 00:10:45 +0100

    Implement queue

---
+++ frelatage/queue/queue.py
@@ -0,0 +1,39 @@
+from frelatage.input.input import Input 
+import numpy as np
+
+class Queue:
+    """
+    The queue contains the list of possible combinations of the corpus entries.
+    """
+    def __init__(self, corpus: list[list[Input]]):
+        self.position = 0
+        self.corpus = corpus
+        self.arguments = self.generate_arguments()
+        self.currents_arguments = self.current_arguments()
+
+    def generate_arguments(self):
+        """
+        Return all possible combinations of inputs.
+        Corpus : [  [Input_1, Input_2],  [Input_3, Input_4]  ]
+        Arguments : [ [Input_1, Input_3],
+                      [Input_1, Input_4],
+                      [Input_2, Input_3],
+                      [Input_2, Input_4],
+                    ]
+        """
+        arguments_list = np.array(np.meshgrid(*self.corpus)).T.reshape(-1, len(self.corpus))
+        arguments = [list(arguments) for arguments in arguments_list]
+        return arguments
+        
+    def current_arguments(self) -> list[Input]:
+        """
+        Return the current arguments
+        """
+        return self.arguments[self.position]
+
+    @property
+    def end(self) -> bool:
+        """
+        Check if we have reached the end of the queue
+        """
+        return self.position >= len(self.arguments)
