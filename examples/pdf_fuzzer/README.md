b38ad3856ac2866cb60e71b36ff49a1f1f8bbee1
Rog3rSm1th <r0g3r5@protonmail.com>
2022-03-21 01:10:30 +0100

    Update PDF fuzzer

---
+++ examples/pdf_fuzzer/README.md
@@ -0,0 +1,5 @@
+## Run the fuzzer
+
+```bash
+./run.sh pdf_fuzzer.py
+```
--- examples/pdf_fuzzer/pdf_fuzzer.py
+++ examples/pdf_fuzzer/pdf_fuzzer.py
@@ -8,9 +8,9 @@ def fuzz_pdf(input_file):
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
     pdfFileObj.close()
 
-# Corpus
-# Small PDF file
-pdf_file = frelatage.Input(file=True, value="file.pdf")
-
-f = frelatage.Fuzzer(fuzz_pdf, [[pdf_file]])
+# Load Corpus
+pdf_file = frelatage.load_corpus("./")
+# Initialize the fuzzer
+f = frelatage.Fuzzer(fuzz_pdf, [pdf_file])
+# Fuzz
 f.fuzz()
