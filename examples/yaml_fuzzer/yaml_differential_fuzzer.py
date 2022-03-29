ae3baac7d4debd398190eff896115c8a2eba737a
Rog3rSm1th <r0g3r5@protonmail.com>
2022-03-29 23:37:25 +0200

    Minor fix in YAML differential fuzzer

--- examples/yaml_fuzzer/yaml_differential_fuzzer.py
+++ examples/yaml_fuzzer/yaml_differential_fuzzer.py
@@ -22,6 +22,6 @@ def fuzz_yaml_load_differential(yaml_file):
 # Load Corpus
 yaml_corpus = frelatage.load_corpus(directory="./")
 # Initialize corpus
-f = frelatage.Fuzzer(fuzz_yaml_load_differential, [yaml_corpus])
+f = frelatage.Fuzzer(fuzz_yaml_load_differential, [yaml_corpus], exceptions_whitelist=(YamlParsingError))
 # Fuzz
 f.fuzz()
